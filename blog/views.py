effd1fbd815e88e583fd568af067923d2906d29f
Nima Dorji Moktan <moktannima2015@gmail.com>
2024-04-30 08:44:36 +0800

    Views and post-detail template updated

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,40 @@ from django.http import HttpResponse
 from datetime import date
 from .models import Post
 # Create your views here.
-# all_posts = [
+#
+
+#Helper funciton
+# def get_date(post):
+#     return post['date']
+
+def index(request):
+    latest_posts = Post.objects.all().order_by('-date')[:3]
+    # sorted_posts = sorted(all_posts,key=get_date)
+    # latest_posts = sorted_posts[-3:]
+    return render(request,'blog/index.html',{'posts':latest_posts})
+    # return HttpResponse('Working from root')
+
+def allPosts(request):
+    # return HttpResponse("All posts page working")
+    all_posts = Post.objects.all().order_by('-date')
+    return render(request,'blog/all-posts.html',
+                  {
+                      'all_posts':all_posts
+                  })
+def post_detail(request,slug):
+    # all_posts = Post.objects.all()
+    # identified_post = next(post for post in all_posts if post['slug'] ==slug)
+    
+    identified_post = get_object_or_404(Post,slug=slug)
+
+    return render(request,"blog/post-detail.html",{
+        'post':identified_post,
+        'post_tags':identified_post.tag.all()
+    })
+
+
+
+#all_posts = [
 #     {
 #         "slug": "hike-in-the-mountains",
 #         "image": "mountains.jpg",
@@ -68,29 +101,3 @@ from .models import Post
 #         """
 #     }
 # ]
-
-#Helper funciton
-# def get_date(post):
-#     return post['date']
-
-def index(request):
-    latest_posts = Post.objects.all().order_by('-date')[:3]
-    # sorted_posts = sorted(all_posts,key=get_date)
-    # latest_posts = sorted_posts[-3:]
-    return render(request,'blog/index.html',{'posts':latest_posts})
-    # return HttpResponse('Working from root')
-
-def allPosts(request):
-    # return HttpResponse("All posts page working")
-    all_posts = Post.objects.all().order_by('-date')
-    return render(request,'blog/all-posts.html',
-                  {
-                      'all_posts':all_posts
-                  })
-def post_detail(request,slug):
-    # all_posts = Post.objects.all()
-    # identified_post = next(post for post in all_posts if post['slug'] ==slug)
-    identified_post = get_object_or_404(Post,slug=slug)
-    return render(request,"blog/post-detail.html",{
-        'post':identified_post
-    })
