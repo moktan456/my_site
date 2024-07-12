1f351d7c3ededb3231f3243c60f0aea198fdf658
Nima Dorji Moktan <moktannima2015@gmail.com>
2024-07-12 21:36:38 +0800

    Views and templates updated

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author,Post,Tag
+from .models import Author,Post,Tag,Comment
 
 
 # Register your models here.
@@ -9,5 +9,9 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug":("title",)}# to slug automatically
     list_filter = ("author","tag","date")
     list_display=("title","author","date")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=("user_name","post")
 admin.site.register(Post,PostAdmin)
 admin.site.register(Tag)
+admin.site.register(Comment,CommentAdmin)
--- blog/views.py
+++ blog/views.py
@@ -62,7 +62,8 @@ class SinglePostView(View):
         context ={
             "post":post,
             "post-tags":post.tag.all(),
-            "comment_form":CommentForm()
+            "comment_form":CommentForm(),
+            "comments":post.comments.all().order_by("-id")
         }
         return render(request,"blog/post-detail.html",context)
 
@@ -79,6 +80,7 @@ class SinglePostView(View):
         context = {
             "post":post,
             "post-tags":post.tag.all(),
-            "comment_form":comment_form
+            "comment_form":comment_form,
+            "comments":post.comments.all().order_by("-id")
         }
         return render(request,"blog/post-detail.html",context)
